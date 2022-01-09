63dca40b2fc4d46f84bf478167a8918afa6a3377
Николай Баишев <baishev.nk@stud.s-vfu.ru>
2022-01-09 20:48:36 +0900

    Add project files.

---
+++ 8_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\C_sharp_projects\example.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            Random rnd = new Random();
+            int S = 0;
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(rnd.Next(0, 11));
+                }
+            }
+
+            using (StreamReader sw = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    int num = Convert.ToInt32(sw.ReadLine());
+                    //Console.WriteLine(num);
+                    S+= num;
+                }
+            }
+
+            Console.WriteLine("Сумма чисел в файле {0}", S);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\C_sharp_projects";
+            Console.WriteLine("Подкаталоги:");
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(dir);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Файлы:");
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            Console.WriteLine();
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine("Подподкаталоги:");
+                string[] subdirs = Directory.GetDirectories(dir);
+                foreach (string subdir in subdirs)
+                {
+                    Console.WriteLine(subdir);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы в подкаталогах:");
+                string[] subfiles = Directory.GetFiles(dir);
+                foreach (string subfile in subfiles)
+                {
+                    Console.WriteLine(subfile);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
